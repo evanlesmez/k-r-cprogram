c929b2f19c3a9e450b4fedc8402c636ae903f418
Evan Lesmez <evan.lesmez@gmail.com>
2025-08-12 23:01:21 -0400

    1-14 solution with char cast

--- problems/1-14.c
+++ problems/1-14.c
@@ -3,32 +3,41 @@
 // Print a histogram of the lengths of words in its input
 
 int main() {
-    int c, i, nwhite;
+    unsigned int c,i, nwhite;
     int nchar[255]; 
 
     nwhite = 0;
-    for (i = 0; i < 255; i++) 
-    {
+    for (i=0; i<256; i++) {
         nchar[i] = 0;
     }
 
     while ((c = getchar()) != EOF) {
-        // Use this line below to debug the int values of chars to ints
-        printf("%c == %i =? %i \n", c, c, c-'0');
-        // a is 97 to z 122 
-        // A is 65 to Z is 90
-        // Actually could just make an array of ints that is one byte large to match the size of char
-        // Then just convert from that in printf
-        ++nchar[c-'0'];
+        // NOTE: evan - using (char) cast is kind of cheating because not taught in book yet.
+        // Instead could have been clever and copied the subtraction method for digits to cast to char.
+        // e.g lower-case letter subtract c - 'a'
+        // 'a' is 97 to 'z' 122 'A' is 65 to 'Z' is 90
+
         if (c == ' ' || c == '\n' || c == '\t')
             ++nwhite;
+        else {
+            ++nchar[c];
+        }
     }
-//    printf("\nChar frequency histogram\n\n");
-//    for (i = 0; i < 36; i++) {
-//        printf("char: %c ", i);
-//        for (int j =0; j < nchar[i]; ++j) 
-//            putchar('-');
-//        putchar('\n');
-//    }
+    printf("\nchar frequency histogram\n");
+    for (i = 0; i < 256; i++) {
+        if (nchar[i] < 1)
+        {
+            continue;
+        }
+        printf("%c ", (char) i);
+        for (int j =0; j < nchar[i]; ++j) 
+            putchar('-');
+        putchar('\n');
+    }
+    printf("whitespace ");
+    for (i = 0; i < nwhite; ++i) 
+            putchar('-');
+    putchar('\n');
+
     return 0;
 }
