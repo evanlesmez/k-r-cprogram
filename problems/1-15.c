5381b76b1ea0f4e7f1dfb1eaac6ab5088a94deeb
Evan Lesmez <evan.lesmez@gmail.com>
2025-08-21 16:04:13 -0400

    1-15 solution: fahr to cels fn sol

--- problems/1-15.c
+++ problems/1-15.c
@@ -2,6 +2,25 @@
 
 // Rewrite the temperature conversion program of Section 1.2 to use a function for conversion.  
 
-int main() {
+float fahr_to_celsius (float temp)
+{
+    return  (5.0/9.0) * (temp -32.0);
+}
+int main() 
+{
+    int lower, upper, step;
+    float fahr, celsius;
+
+    lower = 0;
+    upper = 220;
+    step = 10;
+    
+    fahr = lower;
+    printf("Fahr\tC\n");
+    while (fahr <= upper) {
+        celsius = fahr_to_celsius(fahr);
+        printf("%.0f\t%.2f\n", fahr, celsius);
+        fahr += step;
+    }
     return 0;
 }
